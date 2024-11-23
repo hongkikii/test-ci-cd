18d0a073962bd711a857d43084b8754aae4de1d4
hongkikii <hyangmeeh@icloud.com>
2024-11-24 00:20:10 +0900

    feat: test용 api 추가

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,10 +1,21 @@
 package com.example.demo;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @GetMapping("/")
+    public HttpStatus healthCheck() {
+        return HttpStatus.OK;
+    }
+
+    @GetMapping("/ver")
+    public int getVersion() {
+        return 1;
+    }
 
     @GetMapping("/api")
     public String api() {
