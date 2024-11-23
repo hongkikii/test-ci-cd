9e7a7fd58dec7ec2e29d75c86005de2e5811dd45
hongkikii <hyangmeeh@icloud.com>
2024-11-24 04:01:53 +0900

    test: 배포 실패 시나리오로 api 설정

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -8,20 +8,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    // health check api: 정상/예외 응답 경우에 따라 트래픽을 새로운 서비스로 돌릴지 말지 결정
     @GetMapping("/")
     public HttpStatus healthCheck() {
         return HttpStatus.OK;
     }
 
+    // 애플리케이션 버전 확인 api: 테스트때마다 +1
     @GetMapping("/ver")
     public int getVersion() {
-        return 6;
+        return 7;
     }
 
+    // rollback api: 정상/예외 응답 경우에 따라 다시 기존 서비스로 롤백시킬지를 결정
     @GetMapping("/rollback")
     public ResponseEntity<String> rollback() {
-        return ResponseEntity.ok("OK");
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-//                .body("BAD_REQUEST");
+//        return ResponseEntity.ok("OK");
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("BAD_REQUEST");
     }
 }
